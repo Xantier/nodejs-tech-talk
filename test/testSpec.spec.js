e166944f7d73b48e4e0643bdbfa1072eba7e4a68
Jussi Hallila <jussi@hallila.com>
2015-05-13 08:10:07 +0000

    added gitignore and package.json stuff

---
+++ test/testSpec.spec.js
@@ -0,0 +1,9 @@
+'use strict';
+
+var assert = require("assert")
+
+describe('Very much Test', function(){
+    it('should assert that 1 is one', function(){
+        assert.equal(1, 1, 'One should be equal to one');
+    });
+});
