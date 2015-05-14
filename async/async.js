239e266dbd5c59bd37a713acb456294a7a9144ad
Jussi Hallila <jussi@hallila.com>
2015-05-14 18:21:57 +0000

    added log msg to html server start.

--- async/async.js
+++ async/async.js
@@ -2,8 +2,8 @@
 
 var fs = require('fs');
 
-var retrievedFile = fs.readFile('sometextfile.txt', {encoding: 'utf8'}, function(err, file){
-    if(err) throw new Error('Break everything');
+var retrievedFile = fs.readFile('../sometextfile.txt', {encoding: 'utf8'}, function(err, file){
+    if(err) console.log(err.message);
     console.log('File really retrieved');
     console.log('File contents: ' + file);
 });
--- simpleServers/htmlServer.js
+++ simpleServers/htmlServer.js
@@ -3,11 +3,13 @@
 var http = require('http');
 var fs = require('fs');
 
-fs.readFile('../index.html', function (err, html) {
+fs.readFile('../index.html', {encoding: 'utf8'}, function (err, html) {
     if(err) console.log(err.message);
     http.createServer(function(req, res) {  
         res.writeHeader(200, {"Content-Type": "text/html"});  
         res.write(html);  
         res.end();  
-    }).listen(8080);
+    }).listen(8080, function(){
+        console.log('Server started');
+    });
 });
